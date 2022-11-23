35392e3a33636553a94bb7e4021a5e1f076a243a
Ming <c43892@gmail.com>
2022-11-23 08:59:04 -0500

    a basic version with moving, drawing, erasing and voice chating

---
+++ Assets/Brush/BrushStroke.cs
@@ -0,0 +1,155 @@
+﻿using Normal.Realtime;
+using Normal.Realtime.Serialization;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrushStroke : RealtimeComponent<BrushStrokeModel>
+{
+    [SerializeField]
+    private BrushStrokeMesh _mesh = null;
+
+    // Ribbon State
+    struct RibbonPoint {
+        public Vector3    position;
+        public Quaternion rotation;
+    }
+    private List<RibbonPoint> _ribbonPoints = new List<RibbonPoint>();
+
+    private Vector3    _brushTipPosition;
+    private Quaternion _brushTipRotation;
+    private bool       _brushStrokeFinalized;
+
+    // Smoothing
+    private Vector3    _ribbonEndPosition;
+    private Quaternion _ribbonEndRotation = Quaternion.identity;
+
+    // Mesh
+    private Vector3    _previousRibbonPointPosition;
+    private Quaternion _previousRibbonPointRotation = Quaternion.identity;
+
+    // Unity Events
+    private void Update() {
+        // Animate the end of the ribbon towards the brush tip
+        AnimateLastRibbonPointTowardsBrushTipPosition();
+
+        // Add a ribbon segment if the end of the ribbon has moved far enough
+        AddRibbonPointIfNeeded();
+    }
+
+    // Interface
+    public void BeginBrushStrokeWithBrushTipPoint(Vector3 position, Quaternion rotation) {
+        // Update the model
+        model.brushTipPosition = position;
+        model.brushTipRotation = rotation;
+
+        // Update last ribbon point to match brush tip position & rotation
+        _ribbonEndPosition = position;
+        _ribbonEndRotation = rotation;
+        _mesh.UpdateLastRibbonPoint(_ribbonEndPosition, _ribbonEndRotation);
+    }
+
+    public void MoveBrushTipToPoint(Vector3 position, Quaternion rotation) {
+        model.brushTipPosition = position;
+        model.brushTipRotation = rotation;
+    }
+
+    public void EndBrushStrokeWithBrushTipPoint(Vector3 position, Quaternion rotation) {
+        // Add a final ribbon point and mark the stroke as finalized
+        AddRibbonPoint(position, rotation);
+        model.brushStrokeFinalized = true;
+    }
+
+    private void RibbonPointAdded(RealtimeArray<RibbonPointModel> array, RibbonPointModel ribbonPoint, bool remote)
+    {
+        // Add ribbon point to the mesh
+        _mesh.InsertRibbonPoint(ribbonPoint.position, ribbonPoint.rotation);
+    }
+
+    protected override void OnRealtimeModelReplaced(BrushStrokeModel previousModel, BrushStrokeModel currentModel)
+    {
+        _mesh.ClearRibbon();
+
+        if (previousModel != null)
+        {
+            // Unregister from events
+            previousModel.ribbonPoints.modelAdded -= RibbonPointAdded;
+        }
+
+        if (currentModel != null)
+        {
+            // Replace ribbon mesh
+            foreach (RibbonPointModel ribbonPoint in currentModel.ribbonPoints)
+                _mesh.InsertRibbonPoint(ribbonPoint.position, ribbonPoint.rotation);
+
+            // Update last ribbon point to match brush tip position & rotation
+            _ribbonEndPosition = model.brushTipPosition;
+            _ribbonEndRotation = model.brushTipRotation;
+            _mesh.UpdateLastRibbonPoint(model.brushTipPosition, model.brushTipRotation);
+
+            // Turn off the last ribbon point if this brush stroke is finalized
+            _mesh.skipLastRibbonPoint = model.brushStrokeFinalized;
+
+            // Let us know when a new ribbon point is added to the mesh
+            currentModel.ribbonPoints.modelAdded += RibbonPointAdded;
+        }
+    }
+
+
+    // Ribbon drawing
+    private void AddRibbonPointIfNeeded() {
+        // Only add ribbon points if this brush stroke is being drawn by the local client.
+        if (!realtimeView.isOwnedLocallySelf)
+            return;
+
+        // If the brush stroke is finalized, stop trying to add points to it.
+        if (model.brushStrokeFinalized)
+            return;
+
+        if (Vector3.Distance(_ribbonEndPosition, _previousRibbonPointPosition) >= 0.01f ||
+            Quaternion.Angle(_ribbonEndRotation, _previousRibbonPointRotation) >= 10.0f) {
+
+            // Add ribbon point model to ribbon points array. This will fire the RibbonPointAdded event to update the mesh.
+            AddRibbonPoint(_ribbonEndPosition, _ribbonEndRotation);
+
+            // Store the ribbon point position & rotation for the next time we do this calculation
+            _previousRibbonPointPosition = _ribbonEndPosition;
+            _previousRibbonPointRotation = _ribbonEndRotation;
+        }
+    }
+
+    private void AddRibbonPoint(Vector3 position, Quaternion rotation) {
+        // Create the ribbon point
+        RibbonPointModel ribbonPoint = new RibbonPointModel();
+        ribbonPoint.position = position;
+        ribbonPoint.rotation = rotation;
+        model.ribbonPoints.Add(ribbonPoint);
+
+        // Update the mesh
+        _mesh.InsertRibbonPoint(position, rotation);
+    }
+
+    // Brush tip + smoothing
+    private void AnimateLastRibbonPointTowardsBrushTipPosition() {
+        // If the brush stroke is finalized, skip the brush tip mesh, and stop animating the brush tip.
+        if (model.brushStrokeFinalized) {
+            _mesh.skipLastRibbonPoint = true;
+            return;
+        }
+
+        Vector3 brushTipPosition = model.brushTipPosition;
+        Quaternion brushTipRotation = model.brushTipRotation;
+
+        // If the end of the ribbon has reached the brush tip position, we can bail early.
+        if (Vector3.Distance(_ribbonEndPosition, brushTipPosition) <= 0.0001f &&
+            Quaternion.Angle(_ribbonEndRotation, brushTipRotation) <= 0.01f) {
+            return;
+        }
+
+        // Move the end of the ribbon towards the brush tip position
+        _ribbonEndPosition =     Vector3.Lerp(_ribbonEndPosition, brushTipPosition, 25.0f * Time.deltaTime);
+        _ribbonEndRotation = Quaternion.Slerp(_ribbonEndRotation, brushTipRotation, 25.0f * Time.deltaTime);
+
+        // Update the end of the ribbon mesh
+        _mesh.UpdateLastRibbonPoint(_ribbonEndPosition, _ribbonEndRotation);
+    }
+}
---
+++ Assets/Brush/MouseBrush.cs
@@ -0,0 +1,90 @@
+﻿using Normal.Realtime;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseBrush : MonoBehaviour
+{
+    // Reference to Realtime to use to instantiate brush strokes
+    [SerializeField] private Realtime _realtime;
+
+    // Prefab to instantiate when we draw a new brush stroke
+    [SerializeField] private GameObject _brushStrokePrefab = null;
+
+    [SerializeField] private float BrushDistance = 1.0f;
+
+    // Used to keep track of the current brush tip position and the actively drawing brush stroke
+    private Vector3 _mousePosition;
+    private BrushStroke _activeBrushStroke;
+
+    private void Update()
+    {
+        if (!_realtime.connected)
+            return;
+
+        // Figure out if the trigger is pressed or not
+        bool brushTrigger = Input.GetMouseButton(0);
+
+        if (brushTrigger)
+        {
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                // ereasing
+                var brushStrokePicked = GetMouseTarget();
+                if (brushStrokePicked != null && brushStrokePicked.GetComponent<RealtimeTransform>().isOwnedLocallySelf)
+                    Realtime.Destroy(brushStrokePicked);
+            }
+            else
+            {
+                // drawing
+                if (_activeBrushStroke == null)
+                {
+                    // Instantiate a copy of the Brush Stroke prefab.
+                    GameObject brushStrokeGameObject = Realtime.Instantiate(_brushStrokePrefab.name, ownedByClient: true, useInstance: _realtime);
+
+                    // Grab the BrushStroke component from it
+                    _activeBrushStroke = brushStrokeGameObject.GetComponent<BrushStroke>();
+
+                    // Tell the BrushStroke to begin drawing at the current brush position
+                    _activeBrushStroke.BeginBrushStrokeWithBrushTipPoint(GetBrushPos(), GetBrushOrientation());
+                    _activeBrushStroke.GetComponent<RealtimeTransform>().RequestOwnership();
+                }
+
+                // If the trigger is pressed, and we have a brush stroke, move the brush stroke to the new brush tip position
+                _activeBrushStroke.MoveBrushTipToPoint(GetBrushPos(), GetBrushOrientation());
+            }
+        }
+        else if (_activeBrushStroke != null)
+        {
+            // end up drawing when needed
+            _activeBrushStroke.EndBrushStrokeWithBrushTipPoint(GetBrushPos(), GetBrushOrientation());
+            _activeBrushStroke = null;
+        }
+    }
+
+    //// Utility
+
+    private Vector3 GetBrushPos()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        return ray.GetPoint(BrushDistance);
+    }
+
+    private Quaternion GetBrushOrientation()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        return Quaternion.LookRotation(ray.direction, Vector3.up);
+    }
+
+    private static GameObject GetMouseTarget()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, LayerMask.GetMask("BrushStrokeMesh")))
+        {
+            Transform hitted = hit.transform;
+            return hitted.parent.gameObject;
+        }
+
+        return null;
+    }
+}
